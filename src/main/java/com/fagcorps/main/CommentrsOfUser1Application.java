2afcaf3b4a62ad2009d2ca962a0f8100c384060f
Dev_fag_corp <fagner.compras1.0.0@gmail.com>
2024-12-23 17:05:48 -0300

    Criando Projeto

---
+++ src/main/java/com/fagcorps/main/CommentrsOfUser1Application.java
@@ -0,0 +1,13 @@
+package com.fagcorps.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentrsOfUser1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentrsOfUser1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/fagcorps/main/CommentrsOfUser1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fagcorps.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentrsOfUser1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
